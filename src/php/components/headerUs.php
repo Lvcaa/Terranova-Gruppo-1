8c7f9b57b226006b17d0d48630c770b3d8710521
Luca Galli <19413@studenti.marconiverona.edu.it>
2024-03-21 18:37:17 +0100

    Aggiunto Header About Us

---
+++ src/php/components/headerUs.php
@@ -0,0 +1,17 @@
+<div class="navBarContainer">
+  <a class="title" href="index.php"><span class="clear">Clear</span>Pay</a>
+  <ul id="nav_bar">
+      <div class="social_bar">
+        <img src="../../img/instagram.png" alt="" width="30px" />
+        <img src="../../img/twitter.png" alt="" width="34px" />
+        <img src="../../img/linkedin.png" alt="" width="30px" />
+      </div>
+      <li><a href="">About</a></li>
+      <li><a href="">Our Services</a></li>
+      <li><a href="">Partners</a></li>
+      <li><a href="">Contacts</a></li>
+      <!-- inserire IF ELSE -->
+      <li id="login"><a href="../html/login.html">Login</a></li>
+      <li id="a_register"><a href="../html/register.html">Register</a></li>
+  </ul>
+</div>
