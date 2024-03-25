08eab9787380fec0865c60b9c159f4088dc738bc
Lvcaa <lucagalli05@gmail.com>
2024-03-25 08:34:51 +0100

    Eliminati References Inutilizzati

--- src/php/components/headerHome.php
+++ src/php/components/headerHome.php
@@ -4,10 +4,9 @@
         <img src="../../img/twitter.png" alt="" width="34px" />
         <img src="../../img/linkedin.png" alt="" width="30px" />
       </div>
-      <li><a href="">About</a></li>
+      <li><a href="us.php">About</a></li>
       <li><a href="">Our Services</a></li>
       <li><a href="">Partners</a></li>
-      <li><a href="">Contacts</a></li>
       <!-- inserire IF ELSE -->
       <li id="login"><a href="../html/login.html">Login</a></li>
       <li id="a_register"><a href="../html/register.html">Register</a></li>
