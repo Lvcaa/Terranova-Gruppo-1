261c11e0cd060577ba831f52a44d3e8f4b5cbfc7
Lvcaa <lucagalli05@gmail.com>
2024-03-25 08:34:42 +0100

    Aggiunto id contratto

--- src/php/components/contract.php
+++ src/php/components/contract.php
@@ -1,4 +1,4 @@
-<div class="contract">
+<div class="contract" id="contract1">
   <div class="textContainer">
     <h1 class="contractTitle">Eni Gas & Luce - Bronze</h1>
     <h2 class="contractDate">20/03/2024 - 20/06/2026</h2>
