57927f4c60030dc4a8108e50213cf2e025d73b23
Lvcaa <lucagalli05@gmail.com>
2024-03-27 15:59:46 +0100

    Reindirizzamento MyProfile + Eliminati Commenti

--- src/php/insert_new_users.php
+++ src/php/insert_new_users.php
@@ -1,25 +1,23 @@
 <?php
 
-// Establish connection to the database
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "clearpay";
 
-// Create connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Check if form is submitted
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Check if any required field is empty
+
     if (!empty($_POST['nome']) && !empty($_POST['cognome']) && !empty($_POST['ragione-sociale']) && !empty($_POST['partita-iva']) && !empty($_POST['codice-fiscale']) && !empty($_POST['indirizzo']) && !empty($_POST['civico']) && !empty($_POST['cap']) && !empty($_POST['localita']) && !empty($_POST['provincia']) && !empty($_POST['nazione']) && !empty($_POST['numero-telefonico']) && !empty($_POST['email']) && !empty($_POST['password'])) {
 
-        // Prepare the data for insertion (prevent SQL injection)
         $nome = $conn->real_escape_string($_POST['nome']);
         $cognome = $conn->real_escape_string($_POST['cognome']);
         $ragione_sociale = $conn->real_escape_string($_POST['ragione-sociale']);
@@ -35,16 +33,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $email = $conn->real_escape_string($_POST['email']);
         $password = $conn->real_escape_string($_POST['password']);
 
-        // Hash the password using bcrypt
         $password_hash = password_hash($password, PASSWORD_DEFAULT);
 
-        // SQL query to insert data into the users table
         $sql = "INSERT INTO Utenti (nome, cognome, ragione_sociale, partita_iva, codice_fiscale, indirizzo, civico, cap, localita, provincia, nazione, numero_telefonico, email, password_hash)
         VALUES ('$nome', '$cognome', '$ragione_sociale', '$partita_iva', '$codice_fiscale', '$indirizzo', '$civico', '$cap', '$localita', '$provincia', '$nazione', '$numero_telefonico', '$email', '$password_hash')";
 
-        // Execute the SQL query
         if ($conn->query($sql) === TRUE) {
-            echo "Record inserted successfully";
+            header("Location: myProfile.php");
         } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
         }
@@ -53,6 +48,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
-// Close connection
 $conn->close();
 ?>
